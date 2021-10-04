fcb4578d39045106f94b78f732c9cc6ec40049d8
Akash sarkar <theakashsarkar7@gmail.com>
2021-10-04 14:22:15 +0600

    instagrem clone

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" class="rounded-circle" style="height: 150px; width: 150px;">
+        </div>
+        <div class="col-9">
+
+        </div>
+    </div>
+    
+</div>
+@endsection
